dd6f61ebf6b20f60432ef15875411b632dc89775
charles <cch13@illinois.edu>
2024-10-13 13:32:13 -0500

    quick strucutre/implementation of course, topic classes. question class also frameworked but very skeletony. hopefully the comments help.

---
+++ Question.h
@@ -0,0 +1,23 @@
+#ifndef QUESTION_H
+#define QUESTION_H
+
+#include <string>
+#include <vector>
+
+class Question {
+public:
+    Question();
+    //todo
+
+private:
+    //usage:
+    //get index of your question
+    //use matching index for correct answer
+    //use matching index to ACCESS a vector of possible incorrect answers; pick any amount of them
+    //dont need to use incorrectAnswers_ if it's a short response question
+    std::vector<std::string> questions_;
+    std::vector<std::string> correctAnswers_;
+    std::vector<std::vector<std::string>> incorrectAnswers_;
+};
+
+#endif // QUESTION_H
