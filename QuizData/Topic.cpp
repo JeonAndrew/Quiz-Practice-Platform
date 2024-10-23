5ce1d98969a2163a732ed5d148526892f65df8e7
charles <cch13@illinois.edu>
2024-10-23 14:24:44 -0500

    Finished //TODO in Question class, restructured Topic class to allow Users to Disable Topics later (if they don't want to be quizzed on a certain topic; for instance they may not have learned it yet)

--- QuizData/Topic.cpp
+++ QuizData/Topic.cpp
@@ -1,18 +1,43 @@
 #include "Topic.h"
 
 // Constructor
-Topic::Topic() : proficiency_(0) {}
+Topic::Topic() : proficiency_(10) {}
+
+
+std::string Topic::getName() {
+    return name_;
+}
+
+std::string Topic::setName(std::string in) {
+    name_ = in;
+}
 
 // Increment proficiency
 void Topic::increment() {
     if (proficiency_ < 20) { //can decide on number later
-        ++proficiency_;
+        ++userProficiency_;
     }
+    proficiency_ = userProficiency_;
+    return;
 }
 
 // Decrement proficiency
 void Topic::decrement() {
     if (proficiency_ > 0) {
-        --proficiency_;
+        --userProficiency_;
     }
+    proficiency_ = userProficiency_;
+    return;
+}
+
+void Topic::activate() {
+    proficiency_ = userProficiency_;
+}
+
+void Topic::deactivate() {
+    proficiency_ = -1;
+}
+
+void Topic::addQuestion(Question q) {
+    questions_.push_back(q);
 }
