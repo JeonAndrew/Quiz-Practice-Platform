310ea11925505431d4a4d9a775c94ea7fb72ad7a
andrewjeon <aj55@illinois.edu>
2024-10-29 03:11:11 +0000

    Added a submission status to Quiz

--- quiz-backend/src/Quiz.h
+++ quiz-backend/src/Quiz.h
@@ -10,8 +10,11 @@ class Quiz {
         double getResult() const ;
         std::vector<Question> getQuestions() const;
         void submission(std::vector<std::string>& answers);
+        bool hasSubmitted() const;
 
     private:
         double result_;
         std::vector<Question> questions_;
+        std::vector<bool> questionCorrect(15);
+        bool submissionStatus = false;
 };
