43eb94eb8590bc287febff779cfa9e618079a7b4
scuruchima1 <64672490+scuruchima1@users.noreply.github.com>
2024-11-02 01:07:44 +0000

    added page routing

---
+++ quiz-frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+    return (
+        <div className="home-page">
+            <header>
+                <h1>This will be a home page</h1>
+            </header>
+        </div>
+    );
+}
+
+export default HomePage;
