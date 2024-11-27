0145a18dde6c51b51d0d549d1ea610b0d42a5b95
andrewjeon <aj55@illinois.edu>
2024-11-27 00:30:49 +0000

    made python files

---
+++ quiz-backend/src/User.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class User:
+    def __init__(self, u_id):
+        self.user_id = uid
+        self.performance = deque(max_len = 7)  # To store the last 7 quiz results
+        self.streak = 0
+        self.recent_quiz = None
+
+    def reset_streak(self):
+        self.streak = 0
+
+    def get_streak(self):
+        return self.streak
+
+    def add_to_streak(self):
+        self.streak += 1
+
+    def get_performance(self):
+        return list(self.performance) 
+
+    def set_latest_performance(self):
+        if len(self.performance) >= 7:
+            self.performance.popleft() 
+        self.performance.append(self.recent_quiz.get_result()) 
+
+    def get_quiz(self):
+        return self.recent_quiz
+
+    def set_quiz(self, quiz):
+        self.recent_quiz = quiz
+        self.set_latest_performance()
+
+    def get_user_id(self):
+        return self.user_id
